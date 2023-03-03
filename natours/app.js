71bb7d83aeb689fce70d7e5dbd200b8192ac8777
siddhant paudel <70979294+siddhant7781@users.noreply.github.com>
2023-03-03 16:34:18 +0545

    server routed

--- natours/app.js
+++ natours/app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res
+        .status(200)
+        .json({ message: 'Hello from the server side', app: 'Natours' });
+})
 
+app.post('/', (req, res) => {
+    res.send('you can post now!')
+})
 const port = 3000;
 app.listen(port, () => {
-    console.log(`app running on ${port}`)
+    console.log(`app running on port  ${port}....`);
 })
