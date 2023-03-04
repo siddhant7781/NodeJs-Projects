1b5987492d211a329c39f8417a9b3eea78c00525
siddhant paudel <70979294+siddhant7781@users.noreply.github.com>
2023-03-04 16:51:02 +0545

    learned handling url params

--- natours/app.js
+++ natours/app.js
@@ -1,16 +1,68 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res
-        .status(200)
-        .json({ message: 'Hello from the server side', app: 'Natours' });
+app.use(express.json());
+// app.get('/', (req, res) => {
+//     res
+//         .status(200)
+//         .json({ message: 'Hello from the server side', app: 'Natours' });
+// })
+
+// app.post('/', (req, res) => {
+//     res.send('you can post now!')
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: ' success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
 })
 
-app.post('/', (req, res) => {
-    res.send('you can post now!')
+
+app.get('/api/v1/tours/:id', (req, res) => {
+
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    if (!tour) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'invalid id'
+        })
+    }
+
+    res.status(200).json({
+        status: ' success',
+        data: {
+            tour
+        }
+    })
 })
+
+app.post('/api/v1/tours', (req, res) => {
+    //console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    });
+
+})
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`app running on port  ${port}....`);
