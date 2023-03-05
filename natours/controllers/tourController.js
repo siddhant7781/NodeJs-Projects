36929fc2e31cc389bcbf193c62947de641de21bf
siddhant paudel <70979294+siddhant7781@users.noreply.github.com>
2023-03-05 18:23:33 +0545

    .....

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -14,6 +14,16 @@ exports.checkId = (req, res, next, val) => {
     }
     next();
 }
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: "fail",
+            message: "missing name or price"
+        })
+    }
+    next();
+}
+
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
     res.status(200).json({
--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -4,11 +4,16 @@ const tourController = require('./../controllers/tourController')
 const router = express.Router()
 
 router.param('id', tourController.checkId)
+//create a middleware
+//check if body contains the name and price property
+//if not, send back 400(bad request)
+// add it to the post handler stack
+
 
 router
     .route('/')
     .get(tourController.getAllTours)
-    .post(tourController.createTour)
+    .post(tourController.checkBody, tourController.createTour)
 
 router
     .route('/:id')
