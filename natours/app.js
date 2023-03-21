a7268bd8e648142d93e10a4858ff8c79176c38b1
siddhant paudel <70979294+siddhant7781@users.noreply.github.com>
2023-03-21 22:36:46 +0545

    implemented helmet and rate limit

--- natours/app.js
+++ natours/app.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -9,14 +11,37 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// Middlewares
+//  Gloabal Middlewares
 
+// set security HTTP headers
+app.use(helmet());
+
+// Data Sanitization against NoSQL query injection
+
+
+// Data Sanitization against XSS
+
+//development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-app.use(express.json());
+
+//LIMIT  REQUESTS FROM SAME API OR IP
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests form this IP, please try again in an hour!'
+});
+
+app.use('/api', limiter);
+
+//Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//serving statiic files
 app.use(express.static(`${__dirname}/public `))
 
+//test middlewares
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     console.log(req.headers);
--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -14,7 +14,12 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id)
-
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    }
+    res.cookie('jwt', token, cookieOptions);
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
     res.status(statusCode).json({
         status: 'success',
         token,
