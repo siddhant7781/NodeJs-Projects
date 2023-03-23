7c4d03fd67c72900cc337dcecb839f6da2fd2503
siddhant paudel <70979294+siddhant7781@users.noreply.github.com>
2023-03-23 23:05:07 +0545

    implemented referencing

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -112,7 +112,13 @@ const tourSchema = new mongoose.Schema({
             day: Number
         }
     ],
-    guides: Array,
+    guides: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User'
+        }
+    ]
+
 },
     {
         toJSON: { virtuals: true },
@@ -133,7 +139,9 @@ tourSchema.pre('save', async function (next) {
     const guidesPromises = this.guides.map(async id => await User.findById(id));
     this.guides = await Promise.all(guidesPromises);
     next();
-})
+});
+
+
 // tourSchema.pre('save', function (next) {
 //     console.log('will save document');
 //     next();
