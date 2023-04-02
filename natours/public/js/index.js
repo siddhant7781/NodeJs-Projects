fe1a2629b0c454f13f97b9005d56f6a87a1e6281
siddhantPaudel <siddhantpaudel777@gmail.com>
2023-04-02 08:37:20 +0545

    user photo upload feature completed

--- natours/public/js/index.js
+++ natours/public/js/index.js
@@ -24,9 +24,11 @@ if (logOutBtn) logOutBtn.addEventListener('click', logout);
 if (userDataForm) {
     userDataForm.addEventListener('submit', e => {
         e.preventDefault();
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        updateSettings({ name, email }, 'data');
+        const form = new FormData();
+        form.append('name', document.getElementById('name').value);
+        form.append('email', document.getElementById('email').value);
+        form.append('photo', document.getElementById('pfoto').files[0]);
+        updateSettings(form, 'data');
     })
 }
 
